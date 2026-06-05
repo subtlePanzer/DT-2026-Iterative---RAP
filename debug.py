39837e2d0ca2d3b6b5274422e1038bf9ad1c3e2f
SubtlePanzer <rdl132345@gmail.com>
2026-06-05 12:41:45 +0930

    Setup database setup

---
+++ debug.py
@@ -0,0 +1,11 @@
+import time
+
+verbosity_level = 2
+
+def log(msg: str):
+        if verbosity_level >= 2:
+                print(f'TIME: {round(time.time(), 2)} | {msg}')
+
+        # verbosity 0 - nothing
+        # verbosity 1 - log to file
+        # verbosity 2 - log to terminal
